0fd2176fad7d03c4ad316f514ac3bbd2b21ae117
Nikolay Lukhtura <rrwniko@gmail.com>
2022-06-23 21:50:50 +0200

    my clock-constructor

--- js/script.js
+++ js/script.js
@@ -2,48 +2,44 @@ const startButton = document.querySelector('.start-button');
 const stopButton = document.querySelector('.stop-button');
 const clockArea = document.querySelector('.clock-area');
 
-class Clock {
-    constructor({ template }) {
+function Clock() {
+    this.render = function () {
+        let timer = undefined;
+        let date = new Date();
 
-        let timer;
-
-        function render() {
-            let date = new Date();
-
-            let hours = date.getHours();
-            if (hours < 10)
+        let hours = date.getHours();
+        if (hours < 10) {
+            if (hours < 10) {
                 hours = '0' + hours;
-
-            let mins = date.getMinutes();
-            if (mins < 10)
-                mins = '0' + mins;
-
-            let secs = date.getSeconds();
-            if (secs < 10)
-                secs = '0' + secs;
-
-            let output = template
-                .replace('h', hours)
-                .replace('m', mins)
-                .replace('s', secs);
-
-            clockArea.innerHTML = output;
-
+            }
         }
+        let minutes = date.getMinutes();
+        if (minutes < 10) {
+            if (minutes < 10) {
+                minutes = '0' + minutes;
+            }
+        }
+        let seconds = date.getSeconds();
+        if (seconds < 10) {
+            seconds = '0' + seconds;
+        }
+        clockArea.innerHTML = (`${hours}:${minutes}:${seconds}`);
+    }
 
-        this.stop = function () {
-            clearInterval(timer);
-        };
-
-        this.start = function () {
-            render();
-            timer = setInterval(render, 1000);
-        };
-
+    this.start = function () {
+        this.render();
+        timer = setInterval(this.render, 1000)
     }
 }
 
-let clock = new Clock({ template: 'h:m:s' });
 
-startButton.addEventListener('click', clock.start);
-stopButton.addEventListener('click', clock.stop);
+let clock = new Clock();
+console.log(clock)
+clock.start();
+
+
+
+
+
+// startButton.addEventListener('click', clock.start);
+// stopButton.addEventListener('click', clock.stop);
